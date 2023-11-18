0fb56e3f0352cf02b097e69432f8a22a3566851c
林炘秀 <jenny031054@gmail.com>
2023-11-19 00:49:32 +0800

    完成分頁面

--- main.js
+++ main.js
@@ -2,3 +2,14 @@ import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
 console.log("Hello world!");
+const input = document.querySelector("#name");
+const submit = document.querySelector("#submit");
+submit.addEventListener('click',function(e){
+    e.preventDefault();
+    input.value = "hello 555";
+    let a = input.getAttribute("value");
+    console.log(a); 
+    input.setAttribute("value","155");
+    console.log(input.getAttribute("value"))
+    console.log(input.value); 
+});
